ebd7b175b17d4e3344e03819ae2b77b21fcdfa7d
mstars_lab2_10 <mstars_lab2_10@mstars-lab2-10s-iMac.local>
2022-03-25 14:10:43 +0800

    css changed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React, {Component} from 'react';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  state = {  } 
+  render() { 
+    return (
+      <div className="main-header">
+        <div className='inner'>
+          <h1 className='main-title'>Search App</h1>
+        </div>
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
+
